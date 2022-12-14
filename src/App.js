3748073124ea5b270e2241f21eeb7860f33fa7e9
William Addesso <wsaddesso@gmail.com>
2022-12-13 22:06:24 -0500

    Added react-router-dom and set up routes for Homepage, About, and Products. Began component set up for Homepage, About and Products.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/homepage/Homepage';
+import About from './pages/about/About';
+import Products from './pages/products/Products';
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
