79a47159767c6ed18062c3546a67968b5238cbbd
William Addesso <wsaddesso@gmail.com>
2022-12-18 20:50:43 -0500

    Outlined additional testing functions for ProductCard.

--- src/components/productCard/ProductCard.test.js
+++ src/components/productCard/ProductCard.test.js
@@ -4,8 +4,14 @@ import '@testing-library/jest-dom'; // optional
 import ProductCard from './ProductCard';
 
 describe('ProductCard component', () => {
-  it('renders the product name', () => {
+  it('displays the product name', () => {
     render(<ProductCard name="testName" />);
     expect(screen.getByText('testName')).toBeInTheDocument();
   });
+
+  it('displays a product price', () => {});
+
+  it('displays an image of the product', () => {});
+
+  it('changes the image of the product if the product card is hovered', () => {});
 });
